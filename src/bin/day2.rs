5733042467e6aba6d86910adaa130ac90bcea29a
Ambuj Singh <ambujs89@gmail.com>
2023-07-08 15:20:22 +0530

    feat(day2): Solve day2 problem

---
+++ src/bin/day2.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let input = include_str!("../../inputs/day2.txt");
+
+    let mut horizontal_pos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    // Process the given data
+    let iter = input.lines().map(|line| {
+        if let [instruction, steps, ..] = line.split_whitespace().collect::<Vec<_>>().as_slice() {
+            return (*instruction, *steps);
+        } else {
+            unreachable!("Each line is guaranteed to have an instruction and steps value.")
+        }
+    });
+
+    for (instruction, steps) in iter {
+        let steps = steps.parse::<u32>().unwrap();
+
+        match instruction {
+            "forward" => {
+                horizontal_pos += steps;
+                depth += aim * steps;
+            }
+            "up" => aim -= steps,
+            "down" => aim += steps,
+            _ => unreachable!("The input is guaranteed to match above cases."),
+        }
+    }
+
+    let result = horizontal_pos * depth;
+
+    dbg!(result);
+}
