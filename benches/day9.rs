c7d78158ad8e856f76485a2ff575d1189ccf3d3c
Ambuj Singh <ambujs89@gmail.com>
2023-07-13 03:22:59 +0530

    bench: disable flat sampling

--- benches/day9.rs
+++ benches/day9.rs
@@ -1,5 +1,5 @@
 use aoc_2021::day9::{part1_solution, part2_solution, process_input};
-use criterion::{black_box, criterion_group, criterion_main, Criterion, SamplingMode};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn day9_bench(c: &mut Criterion) {
     let input = include_str!("../inputs/day9.txt");
@@ -7,7 +7,7 @@ fn day9_bench(c: &mut Criterion) {
     let puzzle = process_input(input);
 
     let mut group = c.benchmark_group("Day 9");
-    group.sampling_mode(SamplingMode::Flat).sample_size(500);
+    group.sample_size(500);
 
     group.bench_function("Part 1", |b| b.iter(|| part1_solution(black_box(&puzzle))));
     group.bench_function("Part 2", |b| b.iter(|| part2_solution(black_box(&puzzle))));
